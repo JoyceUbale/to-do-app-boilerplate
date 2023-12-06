78d4babf0c09fa0b3f6fe3c2f7acf8a2a410f4ae
Joyce Ubale <joyceubale@Joyces-MacBook-Air.local>
2023-12-06 15:58:58 +0530

    done

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 // select the elements
 const todolist = document.getElementById("todolist")
-getElementById("todolist")
 const input = document.getElementById("input")
 const addBtn = document.getElementById("button")
-getElementById("button")
 
 // initialise an empty array
 let arr =[];
@@ -12,7 +10,6 @@ let arr =[];
 addBtn.addEventListener("click",()=>{
     // add to the array
     arr.push(input.value);
-    console.log(arr)
     input.value =""
     // call the showlist function
     ShowList()
@@ -24,7 +21,38 @@ function ShowList(){
 // 
 todolist.innerHTML = "";
 // HOF  - foreach  - append all the list items from the array
-arr.forEach((el,i)=>{
-    let listItem = document
+arr.forEach((el,i,arr)=>{
+    let listitem = document.createElement("li")
+        let anchor1 = document.createElement("a")
+        let anchor2 = document.createElement("a")
+
+        listitem.innerText = el;
+        anchor1.innerText = "Edit"
+        anchor1.onclick = ()=>{
+            editList(i);
+        }
+        anchor2.innerText = "Delete "
+        anchor2.style.marginRight = "10px"
+        anchor2.onclick = ()=>{
+            deleteList(i);
+        }
+
+        listitem.append(anchor1,anchor2)
+        todolist.append(listitem)
+        
 })
 }
+//third function
+function editList(i){
+    let newvalue = prompt("Please enter a new value")
+    console.log(newvalue)
+
+    arr.splice(i,1,newvalue)
+    ShowList()
+}
+
+function  deleteList(el,index){
+    arr.splice(index,1)
+    ShowList()
+}
+
